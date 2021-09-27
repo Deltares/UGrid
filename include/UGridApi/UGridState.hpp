f4cd04b11ddacbf5d32929736d99396dfebede15
lucacarniato <carniato.luca@gmail.com>
2021-09-27 11:53:11 +0200

    add doxygen

--- include/UGridApi/UGridState.hpp
+++ include/UGridApi/UGridState.hpp
@@ -12,14 +12,14 @@ namespace ugridapi
     {
         UGridState() = default;
 
-        /// @brief Contructor
-        /// @param nc_file [in] A pointer to a NcFile handler
+        /// @brief Constructor
+        /// @param nc_file [in] A pointer to a NcFile handle
         explicit UGridState(const std::shared_ptr<netCDF::NcFile>& nc_file)
             : m_ncFile(nc_file)
         {
         }
 
-        std::shared_ptr<netCDF::NcFile> m_ncFile;  ///< A pointer to a NcFile handler
+        std::shared_ptr<netCDF::NcFile> m_ncFile;  ///< A pointer to a NcFile handle
         std::vector<ugrid::Mesh1D> m_mesh1d;       ///< A vector containing all Mesh1D instances
         std::vector<ugrid::Network1D> m_network1d; ///< A vector containing all Network1D instances
         std::vector<ugrid::Mesh2D> m_mesh2d;       ///< A vector containing all Mesh2D instances
