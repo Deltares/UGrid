fbb081ab6c5a6a198eab861bcbbdb94c3dfb677a
lucacarniato <carniato.luca@gmail.com>
2021-09-27 10:01:32 +0200

    Remove netcdf-cxx4

--- extern/netcdf/netcdf-cxx/netcdf-cxx4-4.3.1/runCmake.bat
+++
@@ -1,3 +0,0 @@
-rmdir /s xbuild
-rem cmake -S . -B xbuild -G"Visual Studio 16 2019" -DCMAKE_PREFIX_PATH="..\..\netcdf_4_8_0\lib\cmake\netCDF;..\..\..\hdf5\share\cmake\hdf5" --log-level=VERBOSE
-cmake -S . -B xbuild -G"Visual Studio 16 2019" -DCMAKE_PREFIX_PATH="..\..\netcdf_4_8_0\lib\cmake\netCDF;..\..\..\hdf5\share\cmake\hdf5"
