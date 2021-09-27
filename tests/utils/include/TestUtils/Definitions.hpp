58f37349620b137249fa902ac6378350cbb1c30d
lucacarniato <carniato.luca@gmail.com>
2021-09-27 10:03:17 +0200

    Remove TestUtils/Definitions.hpp

--- tests/utils/include/TestUtils/Definitions.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <string>
-#define TEST_FOLDER std::string("D:/ENGINES/io_netcdf_investigation/UGrid/UGrid/tests/data")
-#define TEST_WRITE_FOLDER std::string("D:/ENGINES/io_netcdf_investigation/UGrid/UGrid/tests/data/written_files")
