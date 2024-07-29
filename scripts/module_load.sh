ceb867fbd6387fe1742322569134310d45c4f843
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-07-29 17:40:09 +0200

    Bump versions of modules (CentOS 7 -> Alma 8)

--- scripts/module_load.sh
+++ scripts/module_load.sh
@@ -1,7 +1,8 @@
 # Script that is meant to be sourced in the CI
 # It includes the necessary module loads to build MeshKernel on Deltares Linux machines
 
-module load cmake/3.18.0_gcc9.2.0
-module load gcc/9.2.0
-module load boost/1.73.0_gcc9.2.0
-module load netcdf/v4.7.4_v4.5.3_gcc9.2.0
+module --verbose load gcc/12.2.0_gcc12.2.0
+module --verbose load cmake/3.26.4_gcc12.2.0
+module --verbose load boost/1.83.0_gcc12.2.0
+module --verbose load netcdf/4.9.2_gcc12.2.0
+module --verbose load curl/8.8.0_gcc12.2.0
