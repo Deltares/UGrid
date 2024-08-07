dedc56fe349e872cbf3a539c74cebdc6b0e4ef57
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-08-07 09:51:30 +0200

    Build third party deps in docker build phase

--- scripts/docker/main.sh
+++ scripts/docker/main.sh
@@ -2,20 +2,11 @@
 
 set -e
 
-third_party_dir="/workspace/third_party"
-mkdir -p ${third_party_dir}
-
-chmod +x /workspace/scripts/install_netcdf_cxx4.sh
-
-/workspace/scripts/install_netcdf_cxx4.sh \
-  --work_dir ${third_party_dir}/work \
-  --install_dir ${third_party_dir}/install
-
-export netCDFCxx_DIR=${third_party_dir}/install/netcdf_cxx4/lib64/cmake/netCDF/
+export netCDFCxx_DIR=${THIRD_PARTY_INSTALL_DIR}/netcdf_cxx4/lib64/cmake/netCDF/
 
 build_dir="/workspace/build"
 rm -fr ${build_dir}
-cmake -S . -B ${build_dir} -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${third_party_dir}/install
+cmake -S . -B ${build_dir} -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${THIRD_PARTY_INSTALL_DIR}
 cmake --build ${build_dir} --config Release --parallel
 
 ${build_dir}/bin/UGridApiTests
