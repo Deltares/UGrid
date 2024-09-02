5f90d11bfbbf3ddcd461e9818d990aa37e4fb339
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-09-02 12:31:07 +0200

    Improve namespaces

--- libs/UGridNET/test/src/UGridNETTests.cs
+++ libs/UGridNET/test/src/UGridNETTests.cs
@@ -1,62 +1,55 @@
 using NUnit.Framework;
-using System;
-using System.IO;
 
-
-[TestFixture]
-public class UGridNETTests
+namespace UGridNET.Tests
 {
-    [Test]
-    public void TestMesh1DReadAndInquire()
+
+    [TestFixture]
+    public class UGridNetTests
     {
-        // open
-        //const string file_path = "C:\\Users\\sayed\\work\\repositories\\UGrid\\tests\\data\\AllUGridEntities.nc";
-        const string file_path = "..\\..\\..\\..\\..\\tests\\data\\AllUGridEntities.nc";
-        Console.WriteLine(Path.GetFullPath(file_path));
+        [Test]
+        public void TestMesh1DReadAndInquire()
+        {
+            // open
+            const string filePath = "..\\..\\..\\..\\..\\tests\\data\\AllUGridEntities.nc";
 
-        //const string file_path = "AllUGridEntities.nc";
+            int result = -1;
 
-        int result = -1;
+            int fileMode = -1;
+            result = UGrid.ug_file_read_mode(ref fileMode);
+            Assert.That(result, Is.EqualTo(0));
+            Assert.That(result, Is.EqualTo(0));
 
-        int file_mode = -1;
-        result = UGridCSharpWrapper.ug_file_read_mode(ref file_mode);
-        Assert.That(result, Is.EqualTo(0));
-        Assert.That(result, Is.EqualTo(0));
+            int fileId = -1;
+            result = UGrid.ug_file_open(filePath, fileMode, ref fileId);
+            Assert.That(result, Is.EqualTo(0));
 
-        int file_id = -1;
-        result = UGridCSharpWrapper.ug_file_open(file_path, file_mode, ref file_id);
-        Assert.That(result, Is.EqualTo(0));
+            // get the number of topologies
+            int topologyType = 0;
+            result = UGrid.ug_topology_get_mesh1d_enum(ref topologyType);
+            Assert.That(result, Is.EqualTo(0));
+            int numTopologies = 0;
+            result = UGrid.ug_topology_get_count(fileId, topologyType, ref numTopologies);
+            Assert.That(numTopologies, Is.EqualTo(1));
+            Assert.That(result, Is.EqualTo(0));
 
-        Console.WriteLine("Hoi dikkie dik!!!");
-        Console.WriteLine(file_id);
-        Console.WriteLine(file_mode);
+            // get the mesh
+            var mesh1D = new Mesh1D();
+            const int topologyId = 0;
+            result = UGrid.ug_mesh1d_inq(fileId, topologyId, mesh1D);
+            Assert.That(result, Is.EqualTo(0));
+            Assert.That(mesh1D.num_nodes, Is.EqualTo(25));
+            Assert.That(mesh1D.num_edges, Is.EqualTo(24));
 
-        // get the number of topologies
-        int topology_type = 0;
-        result = UGridCSharpWrapper.ug_topology_get_mesh1d_enum(ref topology_type);
-        Assert.That(result, Is.EqualTo(0));
-        int num_topologies = 0;
-        result = UGridCSharpWrapper.ug_topology_get_count(file_id, topology_type, ref num_topologies);
-        Assert.That(num_topologies, Is.EqualTo(1));
-        Assert.That(result, Is.EqualTo(0));
+            int nameLength = 0;
+            result = UGrid.ug_name_get_length(ref nameLength);
+            Assert.That(result, Is.EqualTo(0));
+            int longNamesLength = 0;
+            result = UGrid.ug_name_get_long_length(ref longNamesLength);
+            Assert.That(result, Is.EqualTo(0));
 
-        // get the mesh
-        var mesh1D = new Mesh1D();
-        const int topology_id = 0;
-        result = UGridCSharpWrapper.ug_mesh1d_inq(file_id, topology_id, mesh1D);
-        Assert.That(result, Is.EqualTo(0));
-        Assert.That(mesh1D.num_nodes, Is.EqualTo(25));
-        Assert.That(mesh1D.num_edges, Is.EqualTo(24));
-
-        int name_length = 0;
-        result = UGridCSharpWrapper.ug_name_get_length(ref name_length);
-        Assert.That(result, Is.EqualTo(0));
-        int long_names_length = 0;
-        result = UGridCSharpWrapper.ug_name_get_long_length(ref long_names_length);
-        Assert.That(result, Is.EqualTo(0));
-
-        // get the data
-        result = UGridCSharpWrapper.ug_mesh1d_get(file_id, 0, mesh1D);
-        Assert.That(result, Is.EqualTo(0));
+            // get the data
+            result = UGrid.ug_mesh1d_get(fileId, 0, mesh1D);
+            Assert.That(result, Is.EqualTo(0));
+        }
     }
 }
