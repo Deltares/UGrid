c90d402d0940e54349503ccea31a3d2bd4baae96
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-10-01 16:49:11 +0200

    Make RUN_SONAR_SCANNER and env var with default var

--- scripts/docker/downlaod_sonar_tools.sh
+++ scripts/docker/downlaod_sonar_tools.sh
@@ -4,11 +4,9 @@ set -e
 
 source /workspace/scripts/docker/utilities.sh
 
-if [[ "${RUN_SONAR_SCANNER}" = "true" ]]; then
-    print_text_box "Download sonar tools"
-    python3 /workspace/scripts/download_sonar_tools.py --save_dir ${SONAR_SAVE_DIR}
-    chmod +x ${SONAR_SAVE_DIR}/build-wrapper-linux-x86/build-wrapper-linux-x86-64
-    chmod +x ${SONAR_SAVE_DIR}/sonar-scanner/bin/sonar-scanner
-    chmod +x ${SONAR_SAVE_DIR}/sonar-scanner/jre/bin/java
-    chmod +x ${SONAR_SAVE_DIR}/sonar-scanner/jre/lib/jspawnhelper
-fi
+print_text_box "Download sonar tools"
+python3 /workspace/scripts/download_sonar_tools.py --save_dir ${SONAR_SAVE_DIR}
+chmod +x ${SONAR_SAVE_DIR}/build-wrapper-linux-x86/build-wrapper-linux-x86-64
+chmod +x ${SONAR_SAVE_DIR}/sonar-scanner/bin/sonar-scanner
+chmod +x ${SONAR_SAVE_DIR}/sonar-scanner/jre/bin/java
+chmod +x ${SONAR_SAVE_DIR}/sonar-scanner/jre/lib/jspawnhelper
