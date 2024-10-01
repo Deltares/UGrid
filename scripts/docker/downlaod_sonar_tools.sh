f05d7a4d6b67dba2b28ce4333a6688366a865941
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-10-01 15:18:14 +0200

    Set permissions recursively to sonar save dir

--- scripts/docker/downlaod_sonar_tools.sh
+++ scripts/docker/downlaod_sonar_tools.sh
@@ -7,9 +7,8 @@ source /workspace/scripts/docker/utilities.sh
 if [[ "${RUN_SONAR_SCANNER}" = "true" ]]; then
     print_text_box "Download sonar tools"
     python3 /workspace/scripts/download_sonar_tools.py --save_dir ${SONAR_SAVE_DIR}
-    chmod +x ${SONAR_SAVE_DIR}/build-wrapper-linux-x86/build-wrapper-linux-x86-64
-    chmod +x ${SONAR_SAVE_DIR}/sonar-scanner/bin/sonar-scanner
-    chmod +x ${SONAR_SAVE_DIR}/sonar-scanner/jre/bin/java
-    ls -ltr ${SONAR_SAVE_DIR}
-    ls -ltr ${SONAR_SAVE_DIR}/build-wrapper-linux-x86
+    chmod -R 776 ${SONAR_SAVE_DIR}
+    #chmod +x ${SONAR_SAVE_DIR}/build-wrapper-linux-x86/build-wrapper-linux-x86-64
+    #chmod +x ${SONAR_SAVE_DIR}/sonar-scanner/bin/sonar-scanner
+    #chmod +x ${SONAR_SAVE_DIR}/sonar-scanner/jre/bin/java
 fi
