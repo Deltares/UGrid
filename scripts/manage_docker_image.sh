be7811768e37ce985f94fb7af5fc14f186008d3a
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-10-07 11:12:05 +0200

    Add script for detection of changes in docker config

---
+++ scripts/manage_docker_image.sh
@@ -0,0 +1,185 @@
+#!/usr/bin/env bash
+
+set -e
+
+function usage() {
+  echo "Usage: $0 \
+--repo_path <REPO_PATH> \
+--server_address <SERVER_ADDRESS> \
+--project_path <PROJECT_PATH> \
+--docker_image_name <DOCKER_IMAGE_NAME> \
+--docker_image_tag <DOCKER_IMAGE_TAG> \
+--docker_file_name <DOCKER_FILE_NAME>"
+}
+
+function parse_args() {
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+    case ${key} in
+    --help)
+      usage
+      exit 0
+      ;;
+    --repo_path)
+      declare -g REPO_PATH=$(realpath "$2")
+      shift 2
+      ;;
+    --server_address)
+      declare -g SERVER_ADDRESS="$2"
+      shift 2
+      ;;
+    --project_path)
+      declare -g PROJECT_PATH="$2"
+      shift 2
+      ;;
+    --docker_image_name)
+      declare -g DOCKER_IMAGE_NAME="$2"
+      shift 2
+      ;;
+    --docker_image_tag)
+      declare -g DOCKER_IMAGE_TAG="$2"
+      shift 2
+      ;;
+    --docker_file_name)
+      declare -g DOCKER_FILE_NAME=$(realpath "$2")
+      shift 2
+      ;;
+    --files_to_check)
+      shift 1
+      declare -ag FILES_TO_CHECK=()
+      while [[ "$#" -gt 0 && "$1" != --* ]]; do
+        FILES_TO_CHECK+=($(realpath "$1"))
+        shift
+      done
+      ;;
+    *)
+      echo "Unknown argument: ${key}"
+      usage
+      exit 1
+      ;;
+    esac
+  done
+
+  # ensure all required arguments are provided
+  if [ -z "${REPO_PATH}" ] ||
+    [ -z "${SERVER_ADDRESS}" ] ||
+    [ -z "${PROJECT_PATH}" ] ||
+    [ -z "${DOCKER_IMAGE_NAME}" ] ||
+    [ -z "${DOCKER_IMAGE_TAG}" ] ||
+    [ -z "${DOCKER_FILE_NAME}" ] ||
+    [ "${#FILES_TO_CHECK[@]}" -eq 0 ]; then
+    echo "One or more missing arguments."
+    usage
+    exit 1
+  fi
+}
+
+function docker_login() {
+  local docker_username=$1
+  local docker_password=$2
+  local server_address=$3
+  echo "${docker_password}" | docker login "${server_address}" --username "${docker_username}" --password-stdin
+}
+
+function docker_logut() {
+  docker logut
+}
+
+function docker_build_and_push() {
+  local full_docker_image_name=$1
+  local docker_file_name=$2
+  # build image
+  docker build -t "${full_docker_image_name}" --progress=plain -f "${docker_file_name}" .
+  # push imgae
+  docker push "${full_docker_image_name}"
+}
+
+function docker_pull() {
+  local full_docker_image_name=$1
+  # pull image
+  docker pull "${full_docker_image_name}"
+}
+
+function get_default_branch_name() {
+  local repo_path=$1
+  echo $(
+    git -C "${repo_path}" symbolic-ref refs/remotes/origin/HEAD |
+      sed 's@^refs/remotes/origin/@@'
+  )
+}
+
+function get_parent_branch_name() {
+  local repo_path=$1
+  echo $(
+    git -C ${repo_path} show-branch |
+      grep '\*' |
+      grep -v $(git -C "${repo_path}" rev-parse --abbrev-ref HEAD) |
+      head -n1 |
+      sed 's/.*\[//g' | sed 's/\].*//g'
+  )
+}
+
+function has_git_diffs() {
+  local repo_path=$1
+  local parent_branch=$2
+  declare -a files_to_check=("${!3}")
+
+  local changes=$(
+    git -C ${repo_path} diff --name-status "${parent_branch}" -- "${files_to_check[@]}"
+  )
+  if [[ -n "${changes}" ]]; then
+    echo "Found changes with respect to branch ${parent_branch}:"
+    echo "${changes}"
+    return 0
+  else
+    echo "No changes found with respect to branch ${parent_branch}"
+    return 1
+  fi
+}
+
+function manage_docker_image() {
+  local repo_path=$1
+  local server_address=$2
+  local project_path=$3
+  local docker_image_name=$4
+  local docker_image_tag=$5
+  local docker_file_name=$6
+  declare -a files_to_check=("${!7}")
+
+  local full_docker_image_name="${server_address}/${project_path}/${docker_image_name}:${docker_image_tag}"
+
+  local parent_branch=$(get_parent_branch_name "${repo_path}")
+  if has_git_diffs "${repo_path}" "${parent_branch}" files_to_check[@]; then
+    echo "Image will be built and pushed"
+    #docker_build_and_push "${full_docker_image_name}" "${docker_file_name}"
+  else
+    # pull
+    echo "Image will be pulled"
+    #docker_pull "${full_docker_image_name}"
+  fi
+}
+
+function main() {
+  parse_args "$@"
+  echo "repo path     : ${REPO_PATH}"
+  echo "server name   : ${SERVER_ADDRESS}"
+  echo "project path  : ${PROJECT_PATH}"
+  echo "image name    : ${DOCKER_IMAGE_NAME}"
+  echo "image tag     : ${DOCKER_IMAGE_TAG}"
+  echo "full name     : ${SERVER_ADDRESS}/${PROJECT_PATH}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
+  echo "file name     : ${DOCKER_FILE_NAME}"
+  echo "files to check: "
+  for file_to_check in "${FILES_TO_CHECK[@]}"; do
+    echo "               ${file_to_check}"
+  done
+
+  manage_docker_image "${REPO_PATH}" \
+    "${SERVER_ADDRESS}" \
+    "${PROJECT_PATH}" \
+    "${DOCKER_IMAGE_NAME}" \
+    "${DOCKER_IMAGE_TAG}" \
+    "${DOCKER_FILE_NAME}" \
+    FILES_TO_CHECK[@]
+}
+
+main "$@"
