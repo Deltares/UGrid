a545ba5c692bd9f5f4eb8fb0260841b685d799e5
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-10-08 10:49:42 +0200

    Fix docker login/logout and improve checks of named args

--- scripts/manage_docker_image.sh
+++ scripts/manage_docker_image.sh
@@ -15,6 +15,7 @@ function usage() {
 }
 
 function parse_args() {
+  local do_exit=false
   while [[ $# -gt 0 ]]; do
     key="$1"
     case ${key} in
@@ -62,10 +63,15 @@ function parse_args() {
         shift
       done
       ;;
+    -* | --*)
+      echo "Unknown named parameter $1"
+      shift 2
+      do_exit=true
+      ;;
     *)
-      echo "Unknown argument: ${key}"
-      usage
-      exit 1
+      echo "Unauthorised positional argument: ${key}"
+      shift
+      do_exit=true
       ;;
     esac
   done
@@ -80,26 +86,63 @@ function parse_args() {
     [ -z "${DOCKER_IMAGE_TAG}" ] ||
     [ -z "${DOCKER_FILE_NAME}" ] ||
     [ "${#FILES_TO_CHECK[@]}" -eq 0 ]; then
-    echo "One or more missing arguments."
+    echo "Missing parameter --."
+    do_exit=true
+  fi
+
+  if [ -z "${REPO_PATH}" ]; then
+    echo "Missing parameter --repo_path."
+    do_exit=true
+  fi
+  if [ -z "${HARBOR_USERNAME}" ]; then
+    echo "Missing parameter --harbor_username."
+    do_exit=true
+  fi
+  if [ -z "${HARBOR_PASSWORD}" ]; then
+    echo "Missing parameter --harbor_password."
+    do_exit=true
+  fi
+  if [ -z "${SERVER_ADDRESS}" ]; then
+    echo "Missing parameter --server_address."
+    do_exit=true
+  fi
+  if [ -z "${PROJECT_PATH}" ]; then
+    echo "Missing parameter --project_path."
+    do_exit=true
+  fi
+  if [ -z "${DOCKER_IMAGE_NAME}" ]; then
+    echo "Missing parameter --docker_image_name."
+    do_exit=true
+  fi
+  if [ -z "${DOCKER_IMAGE_TAG}" ]; then
+    echo "Missing parameter --docker_image_tag."
+    do_exit=true
+  fi
+  if [ "${#FILES_TO_CHECK[@]}" -eq 0 ]; then
+    echo "Missing parameter --files_to_check."
+    do_exit=true
+  fi
+
+  if ${do_exit}; then
     usage
     exit 1
   fi
 }
 
 function docker_login() {
-  local harbor_username=$1
-  local harbor_password=$2
-  local server_address=$3
-  echo ${harbor_password} | docker login "${server_address}" --username "${harbor_username}" --password-stdin
+  local harbor_username="$1"
+  local harbor_password="$2"
+  local server_address="$3"
+  echo "${harbor_password}" | docker login "${server_address}" --username "${harbor_username}" --password-stdin
 }
 
-function docker_logut() {
-  docker logut
+function docker_logout() {
+  docker logout
 }
 
 function docker_build_and_push() {
-  local full_docker_image_name=$1
-  local docker_file_name=$2
+  local full_docker_image_name="$1"
+  local docker_file_name="$2"
   # build image
   docker build -t "${full_docker_image_name}" --progress=plain -f "${docker_file_name}" .
   # push imgae
@@ -113,7 +156,7 @@ function docker_pull() {
 }
 
 function get_default_branch_name() {
-  local repo_path=$1
+  local repo_path="$1"
   echo $(
     git -C "${repo_path}" symbolic-ref refs/remotes/origin/HEAD |
       sed 's@^refs/remotes/origin/@@'
@@ -121,7 +164,7 @@ function get_default_branch_name() {
 }
 
 function get_parent_branch_name() {
-  local repo_path=$1
+  local repo_path="$1"
   echo $(
     git -C ${repo_path} show-branch |
       grep '\*' |
@@ -132,8 +175,8 @@ function get_parent_branch_name() {
 }
 
 function has_git_diffs() {
-  local repo_path=$1
-  local parent_branch=$2
+  local repo_path="$1"
+  local parent_branch="$2"
   declare -a files_to_check=("${!3}")
 
   local changes=$(
@@ -150,12 +193,12 @@ function has_git_diffs() {
 }
 
 function manage_docker_image() {
-  local repo_path=$1
-  local server_address=$2
-  local project_path=$3
-  local docker_image_name=$4
-  local docker_image_tag=$5
-  local docker_file_name=$6
+  local repo_path="$1"
+  local server_address="$2"
+  local project_path="$3"
+  local docker_image_name="$4"
+  local docker_image_tag="$5"
+  local docker_file_name="$6"
   declare -a files_to_check=("${!7}")
 
   local full_docker_image_name="${server_address}/${project_path}/${docker_image_name}:${docker_image_tag}"
@@ -173,18 +216,17 @@ function manage_docker_image() {
 
 function main() {
   parse_args "$@"
-  echo "repo path     : ${REPO_PATH}"
-  echo "server name   : ${SERVER_ADDRESS}"
-  echo "project path  : ${PROJECT_PATH}"
-  echo "user name     : ${HARBOR_USERNAME}"
-  echo "password      : ${HARBOR_PASSWORD}"
-  echo "image name    : ${DOCKER_IMAGE_NAME}"
-  echo "image tag     : ${DOCKER_IMAGE_TAG}"
-  echo "full name     : ${SERVER_ADDRESS}/${PROJECT_PATH}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
-  echo "file name     : ${DOCKER_FILE_NAME}"
-  echo "files to check: "
+  echo "Repo path       : ${REPO_PATH}"
+  echo "Server name     : ${SERVER_ADDRESS}"
+  echo "Project path    : ${PROJECT_PATH}"
+  echo "Username        : ${HARBOR_USERNAME}"
+  echo "Image name      : ${DOCKER_IMAGE_NAME}"
+  echo "Image tag       : ${DOCKER_IMAGE_TAG}"
+  echo "Full image name : ${SERVER_ADDRESS}/${PROJECT_PATH}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
+  echo "File name       : ${DOCKER_FILE_NAME}"
+  echo "Files to check  : "
   for file_to_check in "${FILES_TO_CHECK[@]}"; do
-    echo "               ${file_to_check}"
+    echo "                  ${file_to_check}"
   done
 
   docker_login ${HARBOR_USERNAME} ${HARBOR_PASSWORD} ${SERVER_ADDRESS}
