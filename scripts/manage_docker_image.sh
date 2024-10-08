c9014423c0d1e3d61ce461167b6caf560b45413b
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-10-08 15:34:40 +0200

    Try get_default_branch_name instead of get_parent_branch_name

--- scripts/manage_docker_image.sh
+++ scripts/manage_docker_image.sh
@@ -248,7 +248,8 @@ function manage_docker_image() {
 
   local full_docker_image_name="${server_address}/${project_path}/${docker_image_name}:${docker_image_tag}"
 
-  local parent_branch=$(get_parent_branch_name "${repo_path}")
+  #local parent_branch=$(get_parent_branch_name "${repo_path}")
+  local parent_branch=$(get_default_branch_name "${repo_path}")
   echo "Parent branch: ${parent_branch}"
   if has_git_diffs "${repo_path}" "${parent_branch}" files_to_check[@]; then
     echo "Image will be built and pushed"
