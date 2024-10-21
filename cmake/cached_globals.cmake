cfd68e43dc1da4e77b53e8e856b5a404234c4f3f
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-10-21 14:56:39 +0200

    Fix issues related dir structure in cmake binary dir

--- cmake/cached_globals.cmake
+++ cmake/cached_globals.cmake
@@ -19,14 +19,18 @@ set(GLOB_CURRENT_YEAR ${GLOB_CURRENT_YEAR} CACHE STRING "Current year")
 
 # .NET config
 if(WIN32 AND BUILD_DOTNET_WRAPPER)
-  set(GLOB_LIB_DOTNET_TARGET_FRAMEWORK "netstandard2.0;net481;net8.0" CACHE STRING "Library .NET target framework")
-  set(GLOB_TEST_DOTNET_TARGET_FRAMEWORK "net481;net8.0" CACHE STRING "Test .NET target framework")
-
-  #set(GLOB_LIB_DOTNET_TARGET_FRAMEWORK "netstandard2.0" CACHE STRING "Library .NET target framework")
-  #set(GLOB_TEST_DOTNET_TARGET_FRAMEWORK "net481" CACHE STRING "Test .NET target framework")
-
-  set(GLOB_DOTNET_TARGET_FRAMEWORK_VERSION "v4.8.1" CACHE STRING ".NET target framework version")
+  # .NET SDK
   set(GLOB_DOTNET_SDK "Microsoft.NET.Sdk" CACHE STRING ".NET SDK")
+  # .NET target frameworks
+  # Note: Even if the list consists of a single element, make sure to terminate wirth a semicolon
+  #       For ex: set(LIST_OF_DOTNET_TARGET_FRAMEWORKS "net481;" CACHE STRING "Supported .NET target framework")
+  #       Otherwise, Visual Studio will not create subdir net481 under Release (of whichever build config is used) in the CMake binary dir
+  #       <TargetFramework>net481</TargetFramework> works (notice the singular form in xml element "TargetFramework")
+  #       <TargetFrameworks>net481</TargetFrameworks> does not work (notice the plural form in xml element "TargetFrameworks")
+  #       <TargetFrameworks>net481;</TargetFrameworks> works (notice the plural form in xml element "TargetFrameworks" and the semicolon after "net481")
+  #       Is this a VS bug? "TargetFramework" should be a special case of "TargetFrameworks" when the length of the list is 1.
+  set(GLOB_LIB_DOTNET_TARGET_FRAMEWORKS "netstandard2.0;net481;net5.0" CACHE STRING "Library .NET target framework")
+  set(GLOB_TEST_DOTNET_TARGET_FRAMEWORKS "net481;" CACHE STRING "Test .NET target framework")
+  # .NET language version
   set(GLOB_CSHARP_LANGUAGE_VERSION "7.3" CACHE STRING "C# Compiler Options for language feature rules") 
-  set(GLOB_DOTNET_TARGET_PLATFORM "x64" CACHE STRING ".NET target platform")
 endif()
