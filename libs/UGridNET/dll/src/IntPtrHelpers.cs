ad9a0609def72327e5dee1192c909c1a3f1cf7ac
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-11-26 13:02:23 +0100

    Add doc for IntPtrHelpers

--- libs/UGridNET/dll/src/IntPtrHelpers.cs
+++ libs/UGridNET/dll/src/IntPtrHelpers.cs
@@ -5,16 +5,24 @@ namespace UGridNET
 {
     internal static class IntPtrHelpers
     {
+        /// <summary> Allocates unmanaged memory for an array of elements of the specified type. </summary>
+        /// <typeparam name="T"> The type of elements for which memory is being allocated. </typeparam>
+        /// <param name="count">The number of elements to allocate memory for.</param>
+        /// <returns> A pointer to the allocated unmanaged memory. The caller is responsible for freeing the memory using <see cref="Free"/>. </returns>
+        /// <exception cref="ArgumentException"> Thrown if <paramref name="count"/> is less than or equal to zero. </exception>
         public static IntPtr Allocate<T>(int count) where T : struct
         {
             if (count <= 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(count), "Count must be greater than zero.");
+                throw new ArgumentException("Count must be greater than zero.", nameof(count));
             }
-
-            return Marshal.AllocHGlobal(count * Marshal.SizeOf<T>());
+            int size = count * Marshal.SizeOf<T>();
+            return Marshal.AllocHGlobal(size);
         }
 
+
+        /// <summary> Frees unmanaged memory previously allocated with <see cref="Allocate"/>. </summary>
+        /// <param name="ptr"> A pointer to the unmanaged memory to free.</param>
         public static void Free(IntPtr ptr)
         {
             if (ptr != IntPtr.Zero)
