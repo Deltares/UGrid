7a85675ee7eafb078a5f634d4c1738b7fcdd0488
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2024-12-12 16:34:10 +0100

    Added doc and tetsts for IntPtrExtensions

--- libs/UGridNET/dll/src/IntPtrExtensions.cs
+++ libs/UGridNET/dll/src/IntPtrExtensions.cs
@@ -5,13 +5,26 @@ namespace UGridNET
 {
     namespace Extensions
     {
+        /// <summary>
+        /// Provides extension methods for working with <see cref="IntPtr"/>.
+        /// </summary>
         public static class IntPtrExtensions
         {
-            public static T[] CopyToArray<T>(this IntPtr pointer, int length)
+            /// <summary>
+            /// Copies data from an unmanaged memory pointer to a managed array.
+            /// </summary>
+            /// <typeparam name="T">The type of elements in the array. Must be a value type.</typeparam>
+            /// <param name="pointer">The unmanaged memory pointer.</param>
+            /// <param name="length">The number of elements to copy.</param>
+            /// <returns>A managed array containing the copied data.</returns>
+            /// <exception cref="ArgumentNullException">Thrown when the pointer is null.</exception>
+            /// <exception cref="ArgumentException">Thrown when the length is less than or equal to zero.</exception>
+            /// <exception cref="NotSupportedException">Thrown when the type of elements is not supported.</exception>
+            public static T[] CopyToArray<T>(this IntPtr pointer, int length) where T : struct
             {
                 if (pointer == IntPtr.Zero)
                 {
-                    throw new InvalidOperationException($"IntPtr {nameof(pointer)} is null. Cannot copy to array.");
+                    throw new ArgumentNullException(nameof(pointer), "Pointer is null. Cannot copy a null pointer to a array.");
                 }
 
                 if (length <= 0)
@@ -21,54 +34,63 @@ namespace UGridNET
 
                 var array = new T[length];
 
-                if (typeof(T) == typeof(int))
+                switch (array)
                 {
-                    Marshal.Copy(pointer, array as int[], 0, array.Length);
-                }
-                else if (typeof(T) == typeof(double))
-                {
-                    Marshal.Copy(pointer, array as double[], 0, array.Length);
-                }
-                else if (typeof(T) == typeof(byte))
-                {
-                    Marshal.Copy(pointer, array as byte[], 0, array.Length);
-                }
-                else
-                {
-                    throw new NotSupportedException("Currently only int, double and byte data types are supported.");
+                    case int[] intArray:
+                        Marshal.Copy(pointer, intArray, 0, intArray.Length);
+                        break;
+                    case double[] doubleArray:
+                        Marshal.Copy(pointer, doubleArray, 0, doubleArray.Length);
+                        break;
+                    case byte[] byteArray:
+                        Marshal.Copy(pointer, byteArray, 0, byteArray.Length);
+                        break;
+                    default:
+                        throw new NotSupportedException("Currently only int, double and byte data types are supported.");
                 }
 
                 return array;
             }
 
-            public static void CopyFromArray<T>(this IntPtr pointer, T[] array)
+            /// <summary>
+            /// Copies data from a managed array to an unmanaged memory pointer.
+            /// </summary>
+            /// <typeparam name="T">The type of elements in the array. Must be a value type.</typeparam>
+            /// <param name="pointer">The unmanaged memory pointer.</param>
+            /// <param name="array">The managed array containing the data to copy.</param>
+            /// <exception cref="ArgumentNullException">Thrown when the pointer or array is null.</exception>
+            /// <exception cref="ArgumentException">Thrown when the array is empty.</exception>
+            /// <exception cref="NotSupportedException">Thrown when the type of elements is not supported.</exception>
+            public static void CopyFromArray<T>(this IntPtr pointer, T[] array) where T : struct
             {
-                if (array == null || array.Length == 0)
-                {
-                    throw new ArgumentException("Array must not be null or empty.", nameof(array));
-                }
-
                 if (pointer == IntPtr.Zero)
                 {
-                    throw new InvalidOperationException(
-                        $"IntPtr {nameof(pointer)} is null. Cannot write array {nameof(array)} to pointer.");
+                    throw new ArgumentNullException(nameof(pointer), "Pointer is null.");
                 }
 
-                if (typeof(T) == typeof(int))
+                if (array == null)
                 {
-                    Marshal.Copy(array as int[], 0, pointer, array.Length);
+                    throw new ArgumentNullException(nameof(array), "Array is null.");
                 }
-                else if (typeof(T) == typeof(double))
+
+                if (array.Length == 0)
                 {
-                    Marshal.Copy(array as double[], 0, pointer, array.Length);
+                    throw new ArgumentException("Array is empty.", nameof(array));
                 }
-                else if (typeof(T) == typeof(byte))
+
+                switch (array)
                 {
-                    Marshal.Copy(array as byte[], 0, pointer, array.Length);
-                }
-                else
-                {
-                    throw new NotSupportedException("Currently only int, double and byte data types are supported.");
+                    case int[] intArray:
+                        Marshal.Copy(intArray, 0, pointer, intArray.Length);
+                        break;
+                    case double[] doubleArray:
+                        Marshal.Copy(doubleArray, 0, pointer, doubleArray.Length);
+                        break;
+                    case byte[] byteArray:
+                        Marshal.Copy(byteArray, 0, pointer, byteArray.Length);
+                        break;
+                    default:
+                        throw new NotSupportedException("Currently only int, double and byte data types are supported.");
                 }
             }
         }
