5e413f7799970b519558ae2ea3c380eefab0367f
sayed <ahmad.elsayed@deltares.nl>
2025-02-25 15:50:49 +0100

    Use temp dir to write test output

--- libs/UGridNET/test/src/UGridWriterTests.cs
+++ libs/UGridNET/test/src/UGridWriterTests.cs
@@ -1,17 +1,37 @@
+using System;
 using System.IO;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace UGridNET.Tests
 {
 
+    [TestFixture]
     public class UGridWriterTests
     {
-        private static readonly string TestPath = "C:\\Users\\sayed\\work\\tmp\\ugrid_export_tests";
+        private string testOutputDir;
+        private readonly bool cleanUpWhenDone = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testOutputDir = Path.Combine(Path.GetTempPath(), "UGridNET_tests-" + Guid.NewGuid().ToString());
+            Directory.CreateDirectory(testOutputDir);
+            Console.WriteLine($"Test path {testOutputDir}");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if(cleanUpWhenDone) {
+                Directory.Delete(testOutputDir, true);
+            }
+        }
 
         [Test]
-        public void OpenAndCloseNewFileSucceeds()
+        public void WritingSucceeds()
         {
-            var filePath = Path.Combine(TestPath, "test.nc");
+            var filePath = Path.Combine(testOutputDir, "test.nc");
             string name = "myMesh2D";
             UGridWriter file = null;
             int topologyID = -1;
