c5ef41dc0c0f3b12939a4f5cc54815d62c3851de
sayed <ahmad.elsayed@deltares.nl>
2025-03-09 15:35:52 +0100

    fix backend bugs, cleanups and more tests

--- libs/UGridNET/dll/src/UGridReader.cs
+++ libs/UGridNET/dll/src/UGridReader.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UGridNET.Extensions;
 using System.Runtime.InteropServices;
 using System.Diagnostics.CodeAnalysis;
@@ -138,7 +137,8 @@ namespace UGridNET
             string variableName = "projected_coordinate_system";
             var attributes = GetVariableAttributes(variableName);
             var key = "epsg";
-            if(!attributes.ContainsKey(key)) {
+            if (!attributes.ContainsKey(key))
+            {
                 throw new UGridNETException($"The variable {variableName} does not contain attribute {key}.");
             }
             var valueString = attributes[key];
--- libs/UGridNET/test/src/UGridWriterTests.cs
+++ libs/UGridNET/test/src/UGridWriterTests.cs
@@ -1,9 +1,7 @@
 using System;
 using System.IO;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 using System.Collections.Generic;
-using System.Linq;
 using UGridNET.Extensions;
 
 
@@ -16,11 +14,19 @@ namespace UGridNET.Tests
         private readonly bool cleanUpWhenDone = false;
         private readonly bool appendPathWithGUID = false;
 
+        private readonly Dictionary<string, string> globalAttributes = new Dictionary<string, string>
+        {
+            {"institution", "Deltares"},
+            {"references", "https://www.deltares.nl"} ,
+            {"Conventions", "CF-1.8 UGRID-1.0 Deltares-0.10"}
+        };
+
         [SetUp]
         public void SetUp()
         {
             string testOutputSubDir = "UGridNET_tests";
-            if(appendPathWithGUID) {
+            if (appendPathWithGUID)
+            {
                 testOutputSubDir += '-' + Guid.NewGuid().ToString();
             }
             testOutputDir = Path.Combine(Path.GetTempPath(), testOutputSubDir);
@@ -31,15 +37,16 @@ namespace UGridNET.Tests
         [TearDown]
         public void TearDown()
         {
-            if(cleanUpWhenDone) {
+            if (cleanUpWhenDone)
+            {
                 Directory.Delete(testOutputDir, true);
             }
         }
 
         [Test]
-        public void WritingNativeObjectSucceeds()
+        public void WritingMesh1DSucceeds()
         {
-            string filePath = Path.Combine(testOutputDir, "test.nc");
+            string filePath = Path.Combine(testOutputDir, "mesh1D.nc");
 
             UGridWriter file = null;
 
@@ -49,62 +56,16 @@ namespace UGridNET.Tests
                 NetworkName = "myNetwork".GetRightPaddedNullTerminatedBytes(UGrid.name_long_length),
                 NumNodes = 4,
                 NumEdges = 4,
-                NodeX = new double[]{ 0.0, 1.0, 1.0, 0.0 },
-                NodeY = new double[]{ 0.0, 0.0, 1.0, 1.0 },
-                NodeEdgeID = new int[]{ 0, 0, 0, 0 },
-                NodeEdgeOffset = new double[]{ 0.0, 0.0, 0.0, 0.0 },
-                EdgeX = new double[]{ 0.5, 1.0, 0.5, 0.0 },
-                EdgeY = new double[]{ 0.0, 0.5, 1.0, 0.5 },
-                EdgeNodes = new int[]{ 0, 1, 1, 2, 2, 3, 3, 0 }
+                NodeX = new double[] { 0.0, 1.0, 1.0, 0.0 },
+                NodeY = new double[] { 0.0, 0.0, 1.0, 1.0 },
+                NodeEdgeID = new int[] { 0, 0, 0, 0 },
+                NodeEdgeOffset = new double[] { 0.0, 0.0, 0.0, 0.0 },
+                EdgeX = new double[] { 0.5, 1.0, 0.5, 0.0 },
+                EdgeY = new double[] { 0.0, 0.5, 1.0, 0.5 },
+                EdgeNodes = new int[] { 0, 1, 1, 2, 2, 3, 3, 0 }
             };
 
-            var disposableMesh2D = new DisposableMesh2D
-            {
-                Name = "myMesh2D".GetRightPaddedNullTerminatedBytes(UGrid.name_long_length),
-                NumNodes = 4,
-                NumEdges = 4,
-                NumFaces = 1,
-                NumFaceNodesMax = 4,
-                NodeX = new double[]{ 0.0, 1.0, 1.0, 0.0 },
-                NodeY = new double[]{ 0.0, 0.0, 1.0, 1.0 },
-                NodeZ = new double[]{ 0.0, 0.0, 0.0, 0.0 },
-                EdgeX = new double[]{ 0.5, 1.0, 0.5, 0.0 },
-                EdgeY = new double[]{ 0.0, 0.5, 1.0, 0.5 },
-                EdgeZ = null,
-                EdgeNodes = new int[]{ 0, 1, 1, 2, 2, 3, 3, 0 },
-                EdgeFaces = null,
-                FaceX = new double[]{ 0.5 },
-                FaceY = new double[]{ 0.5 },
-                FaceZ = new double[]{ 0.0 }, //null,
-                FaceNodes = new int[]{ 0, 1, 2, 3 },
-                FaceEdges = new int[]{ 0, 0, 0, 0 },
-                FaceFaces = new int[]{ 0, 0, 0, 0 } //null
-            };
-
-            var otherDisposableMesh2D = new DisposableMesh2D
-            {
-                Name = "myOtherMesh2D".GetRightPaddedNullTerminatedBytes(UGrid.name_long_length),
-                NumNodes = 4,
-                NumEdges = 4,
-                NumFaces = 1,
-                NumFaceNodesMax = 4,
-                NodeX = new double[]{ 0.0, 1.0, 1.0, 0.0 },
-                NodeY = new double[]{ 0.0, 0.0, 1.0, 1.0 },
-                NodeZ = new double[]{ 0.0, 0.0, 0.0, 0.0 },
-                EdgeX = new double[]{ 0.5, 1.0, 0.5, 0.0 },
-                EdgeY = new double[]{ 0.0, 0.5, 1.0, 0.5 },
-                EdgeZ = null,
-                EdgeNodes = new int[]{ 0, 1, 1, 2, 2, 3, 3, 0 },
-                EdgeFaces = null,
-                FaceX = new double[]{ 0.5 },
-                FaceY = new double[]{ 0.5 },
-                FaceZ = new double[]{ 0.0 }, //null,
-                FaceNodes = new int[]{ 0, 1, 2, 3 },
-                FaceEdges = new int[]{ 0, 0, 0, 0 },
-                FaceFaces = new int[]{ 0, 0, 0, 0 } //null
-            };
-
-            string wktStr = 
+            string wktStr =
                 "POINT (30 10)\n"
                 + "LINESTRING (30 10, 10 30, 40 40)\n"
                 + "POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))\n"
@@ -133,22 +94,152 @@ namespace UGridNET.Tests
             try
             {
                 Assert.DoesNotThrow(() => file = new UGridWriter(filePath));
-                //Assert.DoesNotThrow(() => file.AddMesh1D(disposableMesh1D));
+                Assert.DoesNotThrow(() => file.AddMesh1D(disposableMesh1D));
+                Assert.DoesNotThrow(() => file.AddProjectedCoordinateSystem(projectedCoordinateSystem));
+                Assert.DoesNotThrow(() => file.AddGlobalAttribute("source", "Unit test"));
+                Assert.DoesNotThrow(() => file.AddGlobalAttributes(globalAttributes));
+                Assert.DoesNotThrow(() => file.WriteTopologies());
+                Assert.That(file.HasMesh1D, Is.True);
+                Assert.That(file.Mesh1DList.Count, Is.EqualTo(1));
+            }
+            finally
+            {
+                file?.Dispose();
+            }
+        }
+
+        [Test, Order(1)]
+        public void WritingMesh2DSucceeds()
+        {
+            string filePath = Path.Combine(testOutputDir, "mesh2D.nc");
+
+            UGridWriter file = null;
+
+            var disposableMesh2D = new DisposableMesh2D
+            {
+                Name = "myMesh2D".GetRightPaddedNullTerminatedBytes(UGrid.name_long_length),
+                NumNodes = 4,
+                NumEdges = 4,
+                NumFaces = 1,
+                NumFaceNodesMax = 4,
+                NodeX = new double[] { 0.0, 1.0, 1.0, 0.0 },
+                NodeY = new double[] { 0.0, 0.0, 1.0, 1.0 },
+                NodeZ = new double[] { 0.0, 0.0, 0.0, 0.0 },
+                EdgeX = new double[] { 0.5, 1.0, 0.5, 0.0 },
+                EdgeY = new double[] { 0.0, 0.5, 1.0, 0.5 },
+                EdgeZ = null,
+                EdgeNodes = new int[] { 0, 1, 1, 2, 2, 3, 3, 0 },
+                EdgeFaces = null,
+                FaceX = new double[] { 0.5 },
+                FaceY = new double[] { 0.5 },
+                FaceZ = null,
+                FaceNodes = new int[] { 0, 1, 2, 3 },
+                FaceEdges = new int[] { 0, 0, 0, 0 },
+                FaceFaces = null,
+            };
+
+            // var otherDisposableMesh2D = new DisposableMesh2D
+            // {
+            //     Name = "myOtherMesh2D".GetRightPaddedNullTerminatedBytes(UGrid.name_long_length),
+            //     NumNodes = 4,
+            //     NumEdges = 4,
+            //     NumFaces = 1,
+            //     NumFaceNodesMax = 4,
+            //     NodeX = new double[] { 0.0, 1.0, 1.0, 0.0 },
+            //     NodeY = new double[] { 0.0, 0.0, 1.0, 1.0 },
+            //     NodeZ = new double[] { 0.0, 0.0, 0.0, 0.0 },
+            //     EdgeX = new double[] { 0.5, 1.0, 0.5, 0.0 },
+            //     EdgeY = new double[] { 0.0, 0.5, 1.0, 0.5 },
+            //     EdgeZ = null,
+            //     EdgeNodes = new int[] { 0, 1, 1, 2, 2, 3, 3, 0 },
+            //     EdgeFaces = null,
+            //     FaceX = new double[] { 0.5 },
+            //     FaceY = new double[] { 0.5 },
+            //     FaceZ = new double[] { 0.0 }, //null,
+            //     FaceNodes = new int[] { 0, 1, 2, 3 },
+            //     FaceEdges = new int[] { 0, 0, 0, 0 },
+            //     FaceFaces = new int[] { 0, 0, 0, 0 } //null
+            // };
+
+            string wktStr =
+                "POINT (30 10)\n"
+                + "LINESTRING (30 10, 10 30, 40 40)\n"
+                + "POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))\n"
+                + "MULTIPOINT ((10 40), (40 30), (20 20), (30 10))\n"
+                + "GEOMETRYCOLLECTION (\n"
+                + "    POINT (10 40),\n"
+                + "    LINESTRING (30 10, 10 30, 40 40),\n"
+                + "    POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))\n"
+                + ')';
+
+            var projectedCoordinateSystem = new ProjectedCoordinateSystem
+            (
+                epsg: 0,
+                longitudeOfPrimeMeridian: 0.0,
+                semiMajorAxis: 6378137.0,
+                semiMinorAxis: 6356752.314245,
+                inverseFlattening: 298.257223563,
+                name: "proj_coord_sys_name",
+                gridMappingName: "gridMappingName",
+                proj4Params: "proj4Params",
+                epsgCode: "EPSG:0",
+                projectionName: "",
+                wkt: wktStr
+            );
+
+            try
+            {
+                Assert.DoesNotThrow(() => file = new UGridWriter(filePath));
                 Assert.DoesNotThrow(() => file.AddMesh2D(disposableMesh2D));
                 //Assert.DoesNotThrow(() => file.AddMesh2D(otherDisposableMesh2D));
                 Assert.DoesNotThrow(() => file.AddProjectedCoordinateSystem(projectedCoordinateSystem));
-                Assert.DoesNotThrow(() => file.AddGlobalAttribute("Institution", "Deltares"));
-                var otherGlobalAttributes = new Dictionary<string, string> 
-                {
-                    {"references", "https://www.deltares.nl" ,
-                    {"Conventions", "CF-1.8 UGRID-1.0 Deltares-0.10"}
-                };
-                Assert.DoesNotThrow(() => file.AddGlobalAttributes(otherGlobalAttributes));
+                Assert.DoesNotThrow(() => file.AddGlobalAttribute("source", "Unit test"));
+                Assert.DoesNotThrow(() => file.AddGlobalAttributes(globalAttributes));
                 Assert.DoesNotThrow(() => file.WriteTopologies());
-                //Assert.That(file.HasMesh1D, Is.True);
                 Assert.That(file.HasMesh2D, Is.True);
-                //Assert.That(file.Mesh1DList.Count, Is.EqualTo(1));
+                //Assert.That(file.Mesh2DList.Count, Is.EqualTo(2));
                 Assert.That(file.Mesh2DList.Count, Is.EqualTo(1));
+            }
+            finally
+            {
+                file?.Dispose();
+            }
+        }
+
+        [Test, Order(2)]
+        public void ImportingExportedMesh2DSucceeds()
+        {
+            string filePath = Path.Combine(testOutputDir, "mesh2D.nc");
+
+            UGridReader file = null;
+
+            var ExpectedNumNodes = 4;
+            var ExpectedNumEdges = 4;
+            var ExpectedNumFaces = 1;
+            var expectedNodeX = new double[] { 0.0, 1.0, 1.0, 0.0 };
+            var expectedNodeY = new double[] { 0.0, 0.0, 1.0, 1.0 };
+            var expectedEdgeX = new double[] { 0.5, 1.0, 0.5, 0.0 };
+            var expectedEdgeY = new double[] { 0.0, 0.5, 1.0, 0.5 };
+            var expectedEdgeNodes = new int[] { 0, 1, 1, 2, 2, 3, 3, 0 };
+            var expectedFaceX = new double[] { 0.5 };
+            var expectedFaceY = new double[] { 0.5 };
+
+            try
+            {
+                file = new UGridReader(filePath);
+                Assert.That(file.HasMesh2D, Is.True);
+                Assert.That(file.Mesh2DList.Count, Is.EqualTo(1));
+                var mesh2D = file.Mesh2DList[0];
+                Assert.That(mesh2D.num_nodes, Is.EqualTo(ExpectedNumNodes));
+                Assert.That(mesh2D.num_edges, Is.EqualTo(ExpectedNumEdges));
+                Assert.That(mesh2D.num_faces, Is.EqualTo(ExpectedNumFaces));
+                Assert.That(mesh2D.node_x.CopyToArray<double>(mesh2D.num_nodes), Is.EqualTo(expectedNodeX));
+                Assert.That(mesh2D.node_y.CopyToArray<double>(mesh2D.num_nodes), Is.EqualTo(expectedNodeY));
+                Assert.That(mesh2D.edge_x.CopyToArray<double>(mesh2D.num_edges), Is.EqualTo(expectedEdgeX));
+                Assert.That(mesh2D.edge_y.CopyToArray<double>(mesh2D.num_edges), Is.EqualTo(expectedEdgeY));
+                Assert.That(mesh2D.edge_nodes.CopyToArray<int>(mesh2D.num_edges * 2), Is.EqualTo(expectedEdgeNodes));
+                Assert.That(mesh2D.face_x.CopyToArray<double>(mesh2D.num_faces), Is.EqualTo(expectedFaceX));
+                Assert.That(mesh2D.face_y.CopyToArray<double>(mesh2D.num_faces), Is.EqualTo(expectedFaceY));
             }
             finally
             {
