cfbdbb58623a042110e9adc09e1b833e21d74085
BillSenior <bill.senior@deltares.nl>
2025-09-15 17:50:47 +0200

    UGRID-57 Remove use of lib fmt, causes build problems on macos

--- cmake/fetch_contents.cmake
+++ cmake/fetch_contents.cmake
@@ -39,28 +39,3 @@ if(ENABLE_UNIT_TESTING)
 
   enable_testing()
 endif()
-
-if(${USE_LIBFMT})
-  set(LIBFMT_VERSION 11.0.0)
-
-  message(
-    STATUS
-    "${CMAKE_CXX_COMPILER_ID} v.${CMAKE_CXX_COMPILER_VERSION} does not support std::format, "
-    "libfmt v.${LIBFMT_VERSION} will be used instead."
-  )
-
-
-  FetchContent_Declare(
-    fmt
-    GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-    GIT_TAG ${LIBFMT_VERSION}
-  )
-
-  FetchContent_GetProperties(fmt)
-
-  if(NOT fmt_POPULATED)
-    FetchContent_Populate(fmt)
-    add_subdirectory(${fmt_SOURCE_DIR} ${fmt_BINARY_DIR} EXCLUDE_FROM_ALL)
-  endif()
-
-endif()
